83d0e762130934ae288af37ded9900563e0b191c
Otto Seiskari <otto.seiskari@gmail.com>
2021-08-21 20:55:59 +0300

    Change title & description

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,14 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-300">
       <Head>
-        <title>Create Next App</title>
+        <title>oseiskar.github.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20">
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 pt-10 pb-20">
+        <h1 className="mt-3 text-2xl">
+          Selected open source projects by <a href='https://github.com/oseiskar' className="text-blue-700 hover:text-blue-300">oseiskar</a>
+        </h1>
 
         <div className="flex flex-wrap items-center justify-around max-w-7xl mt-6 sm:w-full">
           <GithubProject
