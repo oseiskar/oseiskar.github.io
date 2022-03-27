026d678d72545a9af0f1137ac08e039d21e41dfb
Otto Seiskari <otto.seiskari@gmail.com>
2022-03-27 17:45:52 +0300

    Add OG links to head

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta property="og:url"                content="https://oseiskar.github.io" />
+          <meta property="og:title"              content="Otto Seiskari" />
+          <meta property="og:description"        content="Personal web page. Selected open source projects, publications and computer graphics." />
+          <meta property="og:image"              content="https://oseiskar.github.io/img/webgl-raytracer.png" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
