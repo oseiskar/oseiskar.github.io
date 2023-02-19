a8aaef8f085b832cd153f7e9ca6d97484113d14a
Otto Seiskari <otto.seiskari@gmail.com>
2023-02-19 18:49:37 +0200

    Replace GA with Tinyanalytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-const GOOGLE_ANALYTICS_ID = 'UA-142197325-1';
-
 export default class MyDocument extends Document {
   render() {
     return (
@@ -11,23 +9,8 @@ export default class MyDocument extends Document {
           <meta property="og:title"              content="Otto Seiskari" />
           <meta property="og:description"        content="Personal web page. Selected open source projects, publications and computer graphics." />
           <meta property="og:image"              content="https://oseiskar.github.io/img/webgl-raytracer.png" />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+          {/* Tiny Analytics */}
+          <script defer src="https://app.tinyanalytics.io/pixel/xTQlswQewKufHeJY"></script>
         </Head>
         <body>
           <Main />
