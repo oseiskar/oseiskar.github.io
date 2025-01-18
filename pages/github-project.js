b0695853ee11aeb61dece3fe2c6a9ea7d3d87fd0
Otto Seiskari <otto.seiskari@gmail.com>
2025-01-18 22:55:45 +0200

    New web page style, 2025 (#9)
    * Remove temporary control dashboard
    * Update dependencies
    * New style
    * Build, but do not deploy also on branches
    * Update NodeJS
    * NextJS build fix

--- pages/github-project.js
+++ pages/github-project.js
@@ -1,35 +1,66 @@
 import { FaGithub } from 'react-icons/fa';
 
-export default function GithubProject(props) {
+function ghStars(props, extraClass='') {
+  return <a className={'text-2xl text-gray-500 ' + extraClass } href={ 'https://github.com/' + props.repo }>
+    { props.stars &&
+      <img className="mt-2 lg:mt-1" alt="GitHub Repo stars" src={ 'https://img.shields.io/github/stars/' + props.repo }/>
+    }
+    { !props.stars && <span class="text-lg xl:text-xl inline-block"><FaGithub/></span> }
+  </a>
+}
+
+function sideStars(props) {
+  return <div className="w-40 hidden xl:flex">
+    { ghStars(props, 'ml-8 mt-2') }
+  </div>
+}
+
+export function GithubProject(props) {
   return (
-    <div className="max-w-[38rem] w-full xl:w-[48%] transition duration-500 ease-in-out xl:hover:scale-105 xl:mb-12">
-      <div className="w-full mb-6 mt-4 xl:m-0">
-        <a href={ props.url }
-          className="h-72 w-full bg-auto bg-center rounded-none rounded-t-xl inline-block"
-          style={{ backgroundImage: "url('" + props.image + "')" }}>
-        </a>
-        <div className="-mt-2 xl:h-60 bg-white rounded-b-xl rounded-t-none p-2 md:py-3 shadow-xl bg-gradient-to-r from-gray-100 to-gray-200 text-gray-900 border-t border-gray-200">
-          <div className="mb-8 mt-6 mr-4 ml-8">
-            <div className="full-w flex">
-              <a className="text-2xl font-bold flex-grow hover:text-blue-700" href={ props.url }>{props.name}</a>
-              { props.pypi &&
-              <a className="flex-none hover:text-blue-700 text-2xl text-gray-500" href={ 'https://pypi.python.org/pypi/' + props.pypi }>
-                <img className="mt-1 mr-2" alt="PyPI downloads" src={ 'https://img.shields.io/pypi/dm/' + props.pypi + "?color=%233b82f6" }/>
-              </a>
-              }
-              <a className="flex-none hover:text-blue-700 text-2xl text-gray-500" href={ 'https://github.com/oseiskar/' + props.repo }>
-              { props.stars &&
-                <img className="mt-1" alt="GitHub Repo stars" src={ 'https://img.shields.io/github/stars/oseiskar/' + props.repo + "?color=%233b82f6" }/>
-              }
-              { !props.stars && <FaGithub/> }
-              </a>
-            </div>
-            <p className="mt-4 md:text-lg">
-            {props.description}
-            </p>
+    <div className="flex sm:flex-row flex-col w-full mb-6 mt-4">
+      <div className="flex-1">
+        <div className="pb-2 pr-4 sm:pr-8 xl:pr-12 lg:mt-2">
+          <div className="flex flex-row">
+            <a className="text-lg lg:text-xl font-bold flex-grow" href={ props.url }>{props.name}</a>
+            { ghStars(props, "flex-none xl:hidden") }
           </div>
+          <p className="my-3 text-sm sm:text-base">
+            {props.description}
+          </p>
         </div>
       </div>
+      <a href={ props.url }
+        className="w-full sm:w-64 h-52 sm:h-40 bg-cover bg-center inline-block rounded-lg shadow-lg"
+        style={{ backgroundImage: "url('" + props.image + "')" }}>
+      </a>
+      {sideStars(props)}
     </div>
   )
 }
+
+export function ShortGithubProject(props) {
+  return (
+    <div className="flex flex-row w-full mb-6">
+      <div className="w-40 lg:w-48">
+        <a className="text-lg lg:text-xl font-bold" href={ props.url }>{props.name}</a>
+        <span className="xl:hidden">
+          { ghStars(props) }
+        </span>
+      </div>
+      <div className="flex-1">
+        <div className="">
+          <div className="w-full flex">
+            <a className="flex-none text-2xl text-gray-500" href={ 'https://github.com/oseiskar/' + props.repo }>
+            </a>
+          </div>
+          <p className="text-sm sm:text-base">
+          {props.description}
+          </p>
+        </div>
+      </div>
+      {sideStars(props)}
+    </div>
+  )
+}
+
+export default ShortGithubProject;
