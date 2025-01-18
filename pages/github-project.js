56845023432b779bcd155d6a37d76f1deef16143
Otto Seiskari <otto.seiskari@gmail.com>
2025-01-18 19:33:22 +0200

    New style

--- pages/github-project.js
+++ pages/github-project.js
@@ -1,34 +1,61 @@
 import { FaGithub } from 'react-icons/fa';
 
-export default function GithubProject(props) {
+function ghStars(props, extraClass='') {
+  return <a className={'text-2xl text-gray-500 ' + extraClass } href={ 'https://github.com/oseiskar/' + props.repo }>
+    { props.stars &&
+      <img className="mt-2 lg:mt-1" alt="GitHub Repo stars" src={ 'https://img.shields.io/github/stars/oseiskar/' + props.repo }/>
+    }
+    { !props.stars && <span class="text-lg lg:text-xl hidden lg:inline-block"><FaGithub/></span> }
+  </a>
+}
+
+export function GithubProject(props) {
   return (
-    <div className="max-w-[38rem] w-full xl:w-[48%] transition duration-500 ease-in-out xl:hover:scale-105 xl:mb-12">
-      <div className="w-full mb-6 mt-4 xl:m-0">
-        <a href={ props.url }
-          className="h-72 w-full bg-auto bg-center rounded-none rounded-t-xl inline-block"
-          style={{ backgroundImage: "url('" + props.image + "')" }}>
-        </a>
-        <div className="-mt-2 xl:h-60 bg-white rounded-b-xl rounded-t-none p-2 md:py-3 shadow-xl bg-gradient-to-r from-gray-100 to-gray-200 text-gray-900 border-t border-gray-200">
-          <div className="mb-8 mt-6 mr-4 ml-8">
-            <div className="full-w flex">
-              <a className="text-2xl font-bold flex-grow hover:text-blue-700" href={ props.url }>{props.name}</a>
-              { props.pypi &&
-              <a className="flex-none hover:text-blue-700 text-2xl text-gray-500" href={ 'https://pypi.python.org/pypi/' + props.pypi }>
-                <img className="mt-1 mr-2" alt="PyPI downloads" src={ 'https://img.shields.io/pypi/dm/' + props.pypi + "?color=%233b82f6" }/>
-              </a>
-              }
-              <a className="flex-none hover:text-blue-700 text-2xl text-gray-500" href={ 'https://github.com/oseiskar/' + props.repo }>
-              { props.stars &&
-                <img className="mt-1" alt="GitHub Repo stars" src={ 'https://img.shields.io/github/stars/oseiskar/' + props.repo + "?color=%233b82f6" }/>
-              }
-              { !props.stars && <FaGithub/> }
-              </a>
-            </div>
-            <p className="mt-4 md:text-lg">
+    <div className="flex flex-row w-full mb-6">
+      <div className="flex-1">
+        <div className="pb-6 pr-4 sm:pr-6 lg:mt-4">
+          <a className="text-lg lg:text-xl flex-grow font-bold hover:text-blue-700" href={ props.url }>{props.name}</a>
+          <p className="my-3 text-sm sm:text-base">
             {props.description}
-            </p>
-          </div>
+          </p>
+          <span className="lg:hidden">
+            { ghStars(props) }
+          </span>
         </div>
+      </div>
+      <a href={ props.url }
+        className="w-40 lg:w-64 lg:h-40 bg-cover bg-center inline-block rounded-lg shadow-lg"
+        style={{ backgroundImage: "url('" + props.image + "')" }}>
+      </a>
+      <div className="lg:w-40 hidden lg:flex">
+        { ghStars(props, 'ml-12 mt-4') }
+      </div>
+    </div>
+  )
+}
+
+export function ShortGithubProject(props) {
+  return (
+    <div className="flex flex-row w-full mb-6">
+      <div className="w-40 lg:w-48">
+        <a className="text-lg lg:text-xl font-bold" href={ props.url }>{props.name}</a>
+        <span className="lg:hidden">
+          { ghStars(props) }
+        </span>
+      </div>
+      <div className="flex-1">
+        <div className="">
+          <div className="w-full flex">
+            <a className="flex-none text-2xl text-gray-500" href={ 'https://github.com/oseiskar/' + props.repo }>
+            </a>
+          </div>
+          <p className="text-sm sm:text-base">
+          {props.description}
+          </p>
+        </div>
+      </div>
+      <div className="lg:w-40 hidden lg:flex">
+        { ghStars(props, 'ml-12 mt-2') }
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,151 +1,153 @@
-import Head from 'next/head'
-import GithubProject from './github-project.js'
-import ShortGithubProject from './short-github-project.js'
+import Head from 'next/head';
+import Image from 'next/image';
+import person from '../public/img/otto.jpg';
+import { GithubProject, ShortGithubProject } from './github-project.js';
 import { FaGithub, FaLinkedin, FaOrcid, FaTwitter, FaKaggle, FaStackOverflow, FaMedium, FaInstagram } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-gray-200 to-gray-400">
+    <main className="mx-auto max-w-screen-xl flex pt-4 flex-col lg:flex-row min-h-screen divide-x divide-gray-600 divide-dotted">
       <Head>
         <title>Otto Seiskari - personal web page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-6 md:px-20 pt-2 md:pt-10 pb-20">
-        <div className="flex flex-wrap items-left xl:max-w-7xl mt-6 sm:w-full">
-        </div>
-        <div className="w-full max-w-[38rem] xl:max-w-7xl">
-          <h1 className="text-4xl">
-          Otto Seiskari
-          </h1>
-          <p className="py-2 mb-6 text-gray-600">Programmer, math PhD (D.Sc.), startup CEO @ <a className="text-blue-700 hover:text-blue-400" href="https://www.spectacularai.com/">Spectacular AI</a></p>
-          <div className="flex flex-wrap items-left gap-x-12 gap-y-6 mb-6 text-xl inline-block">
-            <div className="flex flex-wrap items-left gap-12 text-xl inline-block">
-              <a className="hover:text-blue-400" href="https://www.linkedin.com/in/otto-seiskari/"><FaLinkedin className="inline-block"/> Work</a>
-              <a className="hover:text-blue-400" href="https://scholar.google.com/citations?user=6fr78PEAAAAJ">Publications</a>
-            </div>
-            <div className="flex flex-wrap items-left gap-12 mb-8 text-xl inline-block">
-              <a className="hover:text-blue-400 text-gray-500" href="https://www.kaggle.com/oseiskar"><FaKaggle className="inline-block"/> </a>
-              <a className="hover:text-blue-400 text-gray-500" href="https://twitter.com/oseiskar"><FaTwitter className="inline-block"/> </a>
-              <a className="hover:text-blue-400 text-gray-500" href="https://www.instagram.com/oseiskar/"><FaInstagram className="inline-block"/> </a>
-              <a className="hover:text-blue-400 text-gray-500" href="https://stackoverflow.com/users/1426569/oseiskar"><FaStackOverflow className="inline-block"/> </a>
-              <a className="hover:text-blue-400 text-gray-500" href="https://medium.com/@oseiskar"><FaMedium className="inline-block"/> </a>
-              <a className="hover:text-blue-400 text-gray-500" href="https://github.com/oseiskar"><FaGithub className="inline-block"/> </a>
-            </div>
-          </div>
-          <h2 className="text-2xl">
-          Projects
-          </h2>
-          <p className="py-2">Selected open source hobby projects. More on <a href='https://github.com/oseiskar' className="text-blue-700 hover:text-blue-400">Github</a>.</p>
-        </div>
+      {/* 
+        Left Column
+      */}
+      <aside className="flex flex-col mb-10 px-6 lg:px-0">
+        <div className="inline-flex flex-col items-center lg:ml-auto lg:pr-12 lg:w-72">
+          <Image
+            src={person}
+            alt="Person"
+            className="rounded-full object-cover shadow-lg w-72"
+            priority
+            unoptimized
+          />
+          <span className="text-3xl mt-8">Otto Seiskari</span>
+          <span className="my-5 text-gray-600 text-center">Programmer, math PhD (D.Sc.), startup CEO @ <a href="https://www.spectacularai.com/">Spectacular AI</a></span>
 
-        <div className="flex flex-wrap items-center justify-around xl:justify-between xl:max-w-7xl mt-6 sm:w-full">
-          <GithubProject
-            name="Black Hole"
-            repo="black-hole"
-            image="img/black-hole.jpg"
-            stars={true}
-            url="https://oseiskar.github.io/black-hole"
-            description="
-              A real time ray-traced simulation of a Schwarzschild black hole using THREE.js.
-              The contributions of various special and general relativistic effects can be toggled from the GUI."/>
-          <GithubProject
-            name="SIMD Kalman"
-            repo="simdkalman"
-            image="img/simdkalman.png"
-            stars={true}
-            pypi="simdkalman"
-            url="https://simdkalman.readthedocs.io/en/latest/"
-            description="
-              Fast Kalman filters in Python leveraging single-instruction multiple-data vectorization.
-              That is, running n similar Kalman filters on n independent series of observations."/>
-          <GithubProject
-            name="Autosubsync"
-            repo="autosubsync"
-            stars={true}
-            image="https://repository-images.githubusercontent.com/150953681/4b9f925d-37c5-45ec-829f-89b69466ba49"
-            url="https://github.com/oseiskar/autosubsync"
-            description="
-              Automatically synchronize SRT subtitles with audio using AI &amp; Machine Learning.
-              Automatic speed and shift correction, typical synchronization accuracy ~0.15 seconds.
-              Available as a Python package."/>
-          <GithubProject
-            name="Corona Sniffer"
-            repo="corona-sniffer"
-            stars={true}
-            image="https://repository-images.githubusercontent.com/259749452/1f154800-8eb3-11ea-8824-2a31bc68aa84"
-            url="https://github.com/oseiskar/corona-sniffer"
-            description="
-              How anonymous is the Apple/Google BLE contract tracing system (GAEN/ENS)? A proof-of-concept BLE sniffing attack against the system."/>
+          <div className="flex flex-wrap items-left gap-2 text-xl inline-block">
+            <a className="hover:text-blue-400 text-gray-500" href="https://www.linkedin.com/in/otto-seiskari/"><FaLinkedin className="inline-block"/></a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://scholar.google.com/citations?user=6fr78PEAAAAJ"><FaOrcid className="inline-block"/></a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://www.kaggle.com/oseiskar"><FaKaggle className="inline-block"/> </a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://twitter.com/oseiskar"><FaTwitter className="inline-block"/> </a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://www.instagram.com/oseiskar/"><FaInstagram className="inline-block"/> </a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://stackoverflow.com/users/1426569/oseiskar"><FaStackOverflow className="inline-block"/> </a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://medium.com/@oseiskar"><FaMedium className="inline-block"/> </a>
+            <a className="hover:text-blue-400 text-gray-500" href="https://github.com/oseiskar"><FaGithub className="inline-block"/> </a>
+          </div>
         </div>
-        <div className="flex flex-wrap items-center justify-around xl:justify-between xl:max-w-7xl mt-6 sm:w-full">
-          <ShortGithubProject
-            name="WebGL Raytracer"
-            repo="webgl-raytracer"
-            image="img/webgl-raytracer.png"
-            url="https://oseiskar.github.io/webgl-raytracer"
-            description="
-              Run a GPU-powered bidirectional raytracer in the browser and experiment with different
-              scenes and rendering options. GPGPU now possible
-              without spending hours installing CUDA!"/>
-          <ShortGithubProject
-            name="Tree of Life"
-            repo="tree-of-life"
-            image="img/tree-of-life.png"
-            url="https://oseiskar.github.io/tree-of-life"
-            description="
-              An interactive visualization of the Open Tree of Life using d3.js.
-              The entire tree with approximately 2.3 million identified species as
-              leaf nodes is browsable through the application."/>
-          <ShortGithubProject
-            name="JS car"
-            repo="js-car"
-            image="https://repository-images.githubusercontent.com/172246214/c09becb4-7529-400f-9e7a-4c2467156f43"
-            url="https://oseiskar.github.io/js-car"
-            description="
-              JavaScript 2D car dynamics simulation. The physics model is as simple as possible:
-              Frictional forces are applied to two points: the center of the front and
-              rear axle."/>
-          <ShortGithubProject
-            name="QR cloner"
-            repo="qrcloner"
-            image="https://repository-images.githubusercontent.com/418668632/a1fbe5b1-1957-4329-a494-254e19dfac26"
-            url="https://play.google.com/store/apps/details?id=xyz.osei.qrcloner"
-            description="
-              Scan a QR code to move it from your computer screen or a printed paper to your phone.
-              The code is saved on the phone and can later be displayed to QR code scanners."/>
-          <ShortGithubProject
-            name="Puzzles"
-            repo="puzzles"
-            image="https://repository-images.githubusercontent.com/48693032/cc2b23c7-b738-410e-b013-b7eb547c29d1"
-            url="https://oseiskar.github.io/puzzles"
-            description="Archive of ad hoc algorithmic solutions to physical puzzle games. Mostly throwaway-quality code, some of which only runs with Python 2."/>
-          <ShortGithubProject
-            name="AR-3dtris"
-            repo="3dtris"
-            image="img/3dtris.jpg"
-            url="https://play.google.com/store/apps/details?id=xyz.osei.tris"
-            description="
-              Sort out a frustrating mess of falling 3D blocks in
-              Augmented Reality on Anrdoid or the browser. A game and an experiment
-              using WebGL, Emscripten and ARCore."/>
-          <ShortGithubProject
-            name="Mersenne Twister"
-            repo="mersenne-twister"
-            image="img/mersenne-twister.png"
-            url="https://oseiskar.github.io/mersenne-twister"
-            description="
-              Can you spot the difference between the C random number generator
-              and Mersenne Twister?"/>
-          <ShortGithubProject
-            name="Baro floor height"
-            repo="BaroFloorHeight"
-            image="img/baro-floor-height.png"
-            url="https://play.google.com/store/apps/details?id=xyz.osei.baro&hl=en"
-            description="
+      </aside>
+
+      {/* 
+        Right Column (Main Content)
+      */}
+      <section className="flex-1 lg:pl-12 px-6">
+        <h1>Featured projects</h1>
+        <div className="flex flex-wrap items-center justify-around xl:justify-between w-full">
+        <ShortGithubProject
+          name="SIMD Kalman"
+          repo="simdkalman"
+          image="img/simdkalman.png"
+          stars={true}
+          pypi="simdkalman"
+          url="https://simdkalman.readthedocs.io/en/latest/"
+          description="
+            Fast Kalman filters in Python leveraging single-instruction multiple-data vectorization.
+            That is, running n similar Kalman filters on n independent series of observations."/>
+        <ShortGithubProject
+          name="Autosubsync"
+          repo="autosubsync"
+          stars={true}
+          image="https://repository-images.githubusercontent.com/150953681/4b9f925d-37c5-45ec-829f-89b69466ba49"
+          url="https://github.com/oseiskar/autosubsync"
+          description="
+            Automatically synchronize SRT subtitles with audio using AI &amp; Machine Learning.
+            Automatic speed and shift correction, typical synchronization accuracy ~0.15 seconds.
+            Available as a Python package."/>
+        <ShortGithubProject
+          name="Corona Sniffer"
+          repo="corona-sniffer"
+          stars={true}
+          image="https://repository-images.githubusercontent.com/259749452/1f154800-8eb3-11ea-8824-2a31bc68aa84"
+          url="https://github.com/oseiskar/corona-sniffer"
+          description="
+            How anonymous is the Apple/Google BLE contract tracing system (GAEN/ENS)? A proof-of-concept BLE sniffing attack against the system."/>
+        <GithubProject
+          name="Black Hole"
+          repo="black-hole"
+          image="img/black-hole.jpg"
+          stars={true}
+          url="https://oseiskar.github.io/black-hole"
+          description="
+            A real time ray-traced simulation of a Schwarzschild black hole using THREE.js.
+            The contributions of various special and general relativistic effects can be toggled from the GUI."/>
+        <GithubProject
+          name="WebGL Raytracer"
+          repo="webgl-raytracer"
+          image="img/webgl-raytracer.png"
+          stars={true}
+          url="https://oseiskar.github.io/webgl-raytracer"
+          description="
+            Run a GPU-powered bidirectional raytracer in the browser and experiment with different
+            scenes and rendering options. GPGPU now possible
+            without spending hours installing CUDA!"/>
+        <GithubProject
+          name="JS car"
+          repo="js-car"
+          stars={true}
+          image="https://repository-images.githubusercontent.com/172246214/c09becb4-7529-400f-9e7a-4c2467156f43"
+          url="https://oseiskar.github.io/js-car"
+          description="
+            JavaScript 2D car dynamics simulation. The physics model is as simple as possible:
+            Frictional forces are applied to two points: the center of the front and
+            rear axle."/>
+        <GithubProject
+          name="Tree of Life"
+          repo="tree-of-life"
+          image="img/tree-of-life.png"
+          stars={true}
+          url="https://oseiskar.github.io/tree-of-life"
+          description="
+            An interactive visualization of the Open Tree of Life using d3.js.
+            The entire tree with approximately 2.3 million identified species as
+            leaf nodes is browsable through the application."/>
+        <GithubProject
+          name="Puzzles"
+          repo="puzzles"
+          image="https://repository-images.githubusercontent.com/48693032/cc2b23c7-b738-410e-b013-b7eb547c29d1"
+          url="https://oseiskar.github.io/puzzles"
+          description="Archive of ad hoc algorithmic solutions to physical puzzle games. Mostly throwaway-quality code, some of which only runs with Python 2."/>
+        <GithubProject
+          name="Mersenne Twister"
+          repo="mersenne-twister"
+          image="img/mersenne-twister.png"
+          url="https://oseiskar.github.io/mersenne-twister"
+          description="
+            Can you spot the difference between the C random number generator
+            and Mersenne Twister?"/>
+        {
+          false && <div>
+            <ShortGithubProject
+              name="Baro floor height"
+              repo="BaroFloorHeight"
+              image="img/baro-floor-height.png"
+              url="https://play.google.com/store/apps/details?id=xyz.osei.baro&hl=en"
+              description="
               An Andoid app for measuring the height of storeys and buildings using the barometer."/>
+            <ShortGithubProject
+              name="QR cloner"
+              repo="qrcloner"
+              image="https://repository-images.githubusercontent.com/418668632/a1fbe5b1-1957-4329-a494-254e19dfac26"
+              url="https://github.com/oseiskar/qr-cloner"
+              description="
+                Scan a QR code to move it from your computer screen or a printed paper to your phone.
+                The code is saved on the phone and can later be displayed to QR code scanners."/>
+          </div>
+        }
         </div>
-      </main>
-    </div>
-  )
+      </section>
+    </main>
+  );
 }
