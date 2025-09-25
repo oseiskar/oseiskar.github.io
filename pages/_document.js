79e533fd8a326d5840ad60ac8b793c43ee2a2b0f
Otto Seiskari <otto.seiskari@gmail.com>
2025-09-25 21:50:19 +0300

    Drop TinyAnalytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,6 @@ export default class MyDocument extends Document {
           <meta property="og:title"              content="Otto Seiskari" />
           <meta property="og:description"        content="Personal web page. Selected open source projects, publications and computer graphics." />
           <meta property="og:image"              content="https://oseiskar.github.io/img/webgl-raytracer.png" />
-          {/* Tiny Analytics */}
-          <script defer src="https://app.tinyanalytics.io/pixel/xTQlswQewKufHeJY"></script>
         </Head>
         <body>
           <Main />
